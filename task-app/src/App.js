258941559c7104f7efebadffa240b7c0eeea0d52
TatsuyaSSK <ssktty514@gmail.com>
2023-05-27 20:40:05 +0900

    update add.js

--- task-app/src/App.js
+++ task-app/src/App.js
@@ -1,10 +1,53 @@
 import "./App.css";
-import { DragDropContext } from "react-beautiful-dnd";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+
+const items = [
+  {
+    id: "item-0",
+    content: "item 0",
+  },
+  {
+    id: "item-1",
+    content: "item 1",
+  },
+  {
+    id: "item-2",
+    content: "item 2",
+  },
+  {
+    id: "item-3",
+    content: "item 3",
+  },
+  {
+    id: "item-4",
+    content: "item 4",
+  },
+];
 
 function App() {
   return (
     <div>
-      <DragDropContext></DragDropContext>
+      <DragDropContext>
+        <Droppable droppableId="dropppable">
+          {(provided, snapshot) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              {items.map((item, index) => (
+                <Draggable key={item.id} draggableId={item.id} index={index}>
+                  {(provided, snapshot) => (
+                    <div
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                    >
+                      {item.content}
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   );
 }
