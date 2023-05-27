eadc57f9b84a994b0a3ced93491bd64655c095a2
TatsuyaSSK <ssktty514@gmail.com>
2023-05-27 20:23:29 +0900

    initial commit

---
+++ task-app/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import { DragDropContext } from "react-beautiful-dnd";
+
+function App() {
+  return (
+    <div>
+      <DragDropContext></DragDropContext>
+    </div>
+  );
+}
+
+export default App;
